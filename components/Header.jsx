59d8ec323da0e13da5500c05c5c2bb352f7e6566
shai9 <destronan5@gmail.com>
2023-03-31 10:38:36 +0300

    Create navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,36 @@
-export default (Haeder) => {
+import css from '../styles/Header.module.css'
+import Image from 'next/image'
+import Logo from '../assets/Logo.png';
+import { BiCartAlt } from 'react-icons/bi'
+
+export default (Header) => {
   return(
-    <div>Hello from Header</div>
+    <div className="css.header">
+
+      {/* Logo side */}
+
+      <div className='css.logo'>
+        <Image src={Logo} alt='' width={50} height={50} />
+        <span>GrocerGo</span>
+      </div>
+
+      {/* Menu */}
+
+      <div className='css.menu'>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* Cart side */}
+      
+      <div className='css.cartside'>
+        <div className='css.cart'>
+          <BiCartAlt />
+        </div>
+      </div>
+    </div>
   )
 }
