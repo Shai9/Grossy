5777481c84b10eed7cb8e335ccb3b02baf5ddedf
shai9 <destronan5@gmail.com>
2023-04-01 14:02:04 +0300

    Start on the hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,6 +1,10 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Banana from '../assets/crop.png';
+import HeroImage from '../assets/Her.png'
 import css from '../styles/Hero.module.css'
+import { IoIosCall } from "react-icons/io";
+import Oranges from '../assets/oranges.jpg'
+
 
 const Hero = () => {
   return (
@@ -17,15 +21,38 @@ const Hero = () => {
           <span>We provide</span>
           <span>Delivery within</span>
           <span>
-            <span style={{color:"green"}}>30 minutes</span>
+            <span style={{color:"green"}}>30 min</span>
           </span>
         </div>
+        <span className={css.minitext}>
+        Shop at our grocery store for the best quality produce. Enjoy same-day delivery straight to your doorstep
+        </span>
+        <button className={`btn ${css.btn}`}>
+          Shop now
+        </button>
       </div> 
 
       {/* Right side */}
 
       <div className={css.rightside}>
-        1
+        <div className={css.Imagecontainer}>
+          <Image src={HeroImage} alt="" layout="fill" />
+        </div>
+        <div className={css.contactUs}>
+          <span>Call us</span>
+          <div>
+          <IoIosCall />
+          </div>
+        </div>
+        <div className={css.Veges}>
+          <div>
+            <Image src={Oranges} alt="" objectFit="cover" layout="fill"/>
+          </div>
+          <div className="css.details">
+            <span>Navel oranges</span>
+            <span>Ksh 50</span>
+          </div>
+        </div>
       </div>
     </div>
   )
